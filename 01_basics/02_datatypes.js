bd691b7067f9d9190c9ec3e36ff7e2bf2097fb59
shivamp8644p <shivamp8644@gmail.com>
2026-07-17 05:33:30 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,4 +1,7 @@
 "use strict"; //treat all js code as newer version.
 //alert(9+9) we are using node js not the browser.
 
-console.log(typeof"shivam");
+// console.log(typeof"shivam");
+// console.log(typeof null);
+// console.log(typeof"undefined");
+ console.log(typeof 11)
