ea4d58e862dece2af7c6412283d2c17105155681
shivamp8644p <shivamp8644@gmail.com>
2026-07-17 05:24:56 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "shivam@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountstate;
+
+//accountId=2
+
+accountEmail="hc@hj.com"
+accountPassword="986778"
+accountCity="jaipur"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountstate ])
