15bbc42b981c67e2c6f29550b7dc049cea5408c8
shivamp8644p <shivamp8644@gmail.com>
2026-07-17 10:32:21 +0000

    conversionopetation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -10,5 +10,35 @@
 
 let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+//*************** OPERATIONS***********/
+
+let value=3
+let negValue=-value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2-2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+
+
+let str1="hello"
+let str2=" shivam"
+let str3= str1 + str2
+
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+3)
+console.log(1+2+"3")
+
+console.log(+true)
+console.log(+"")
