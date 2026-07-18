b0c58ac239441b2f15a424d09879f51331f77ff1
shivamp8644p <shivamp8644@gmail.com>
2026-07-18 08:00:13 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,31 @@
+//DATES
+
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate)
+
+// let myCreatedDate = new Date(2026,6,18)
+// let myCreatedDate1 = new Date(2026,6,18,1,6)
+// let myCreatedDate2 = new Date("2026-06-18")
+ let myCreatedDate3 = new Date("18-07-2026")
+// console.log(myCreatedDate.toDateString())
+// console.log(myCreatedDate1.toLocaleString())
+// console.log(myCreatedDate2.toLocaleString())
+// console.log(myCreatedDate3.toLocaleString())
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(myCreatedDate3.getTime())
+
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth()+1)
+console.log(newDate.getDay())
