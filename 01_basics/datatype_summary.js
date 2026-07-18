99b2bb9247fc8d44f496de1784a9f19dded0080a
shivamp8644p <shivamp8644@gmail.com>
2026-07-18 04:43:07 +0000

    datatypesandmemory

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -33,8 +33,37 @@ let myObj = {                       //objects
 const myFunction = function(){              //function
     console.log("hello world");
 }
-console.log(typeof myFunction)
-console.log(typeof id)
-console.log(typeof anotherId)
-console.log(typeof bigNumber)
-console.log(typeof heros)
+// console.log(typeof myFunction)
+// console.log(typeof id)
+// console.log(typeof anotherId)
+// console.log(typeof bigNumber)
+// console.log(typeof heros)
+
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Memory
+
+//stack(primitive), Heap(Non-Primitive)
+
+let myYoutubename = "shivampandey.com"
+
+let anothername = "hiteshchaudhry.com"
+anothername = "chaiaurcode"
+
+console.log(anothername)
+console.log(myYoutubename)
+
+let user1 = {
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+
+let user2 = user1
+
+user2.email = "hitesh@google.com"
+
+console.log(user1.email)
+console.log(user2.email)
