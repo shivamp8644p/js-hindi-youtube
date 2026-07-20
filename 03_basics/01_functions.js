5563a8ae1493582b809d4f541c0dfce27cbe14e5
shivamp8644p <shivamp8644@gmail.com>
2026-07-20 08:36:37 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,26 @@
+// function sayMyName(){
+//     console.log("s");
+//     console.log("h");
+//     console.log("i");
+//     console.log("v");
+//     console.log("a");
+//     console.log("m");
+// }
+// sayMyName()
+
+
+function addTwoNumbers(num1, num2){
+// let result = num1+num2
+// return result            //or next tarika
+return num1+num2
+}
+
+const result = addTwoNumbers(8,9)
+// console.log("result: ", result);
+
+
+function loginUserMessage(username){
+return `${username} just logged in`
+}
+
+console.log(loginUserMessage("shivam"))
