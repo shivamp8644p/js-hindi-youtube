0bde9402547ae58521f927708bf3850796c1c6ee
shivamp8644p <shivamp8644@gmail.com>
2026-07-20 07:50:35 +0000

    04changes

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -27,12 +27,26 @@ const obj3={5:'a', 6:'b'}
 // const obj4 = Object.assign({},obj1,obj2,obj3)
 
 const obj4 = {...obj1, ...obj2, ...obj3}
-console.log(obj4)
+// console.log(obj4)
 
-console.log(tinderUser)
+// console.log(tinderUser)
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('name'))
+// console.log(tinderUser.hasOwnProperty('name'))
+
+
+
+const course = {
+coursename: 'js in hindi',
+price:'999',
+courseInstructor: 'hitesh'
+}
+
+//course.courseInstructor
+
+const {courseInstructor} = course
+
+console.log(courseInstructor);
