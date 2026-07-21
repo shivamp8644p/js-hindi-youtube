5539ef0a8e1ab9fb149ad2f74ba12108c529be1f
shivam <shivamp8644@gmail.com>
2026-07-21 09:37:14 +0530

    01_function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -23,4 +23,35 @@ function loginUserMessage(username){
 return `${username} just logged in`
 }
 
-console.log(loginUserMessage("shivam"))
+// console.log(loginUserMessage("shivam"))
+
+
+
+
+function calculateCartPrice(...num1){
+return num1
+} 
+
+// console.log(calculateCartPrice(200, 400, 600, 800)) 
+
+const user = {
+    username: "shivam",
+    price: 500,
+}
+
+function handleObject(anyobject){
+console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "shivam",
+    price: 50,
+})
+
+const myNewArray = [145,23,635,44,50,66]
+
+function returnSecondValue(getArray){
+return getArray[1]
+}
+console.log(returnSecondValue(myNewArray))
